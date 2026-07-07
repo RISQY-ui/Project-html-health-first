142f8852cfe123a05acb17444db941728822362a
M.FAHRIZAL RISQY <rizkifaris703@gmail.com>
2026-07-07 15:37:35 +0700

    Add files via upload

---
+++ belajar js.js
@@ -0,0 +1,12 @@
+function infoAir() {
+    alert("Water helps the body stay healthy and prevents dehydration.");
+}
+
+function infoOlahraga() {
+    alert("Exercise helps the heart stay healthier and the body stronger.");
+}
+
+function ubahWarna() {
+    document.body.style.background = "#e8f8f5";
+    document.getElementById("judul").innerHTML = "Stay Healthy Today!";
+}
